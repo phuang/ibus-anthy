088d2d9167bc7492c0d2896e4659cc37c82c17d3
fujiwarat <takao.fujiwara1@gmail.com>
2009-10-21 15:47:17 +0900

    Fix string errors #582. Contributed fr.po by Humbert Julien <julroy67@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -2,12 +2,13 @@
 set -e
 set -x
 
-autopoint --force
-libtoolize --automake --copy --force
-aclocal -I m4 --force
-autoheader --force
-automake --add-missing --copy --force
-autoconf --force
+autopoint --force || exit 1
+libtoolize --automake --copy --force || exit 1
+intltoolize --copy --force || exit 1
+aclocal -I m4 --force || exit 1
+autoheader --force || exit 1
+automake --add-missing --copy --force || exit 1
+autoconf --force || exit 1
 export CFLAGS="-Wall -g -O0 -Wl,--no-undefined"
 export CXXFLAGS="$CFLAGS"
-./configure --enable-maintainer-mode $*
+./configure --enable-maintainer-mode $* || exit 1
--- engine/engine.py
+++ engine/engine.py
@@ -138,25 +138,25 @@ class Engine(ibus.EngineBase):
         input_mode_prop = ibus.Property(key=u"InputMode",
                                         type=ibus.PROP_TYPE_MENU,
                                         label=u"あ",
-                                        tooltip=_(u"Switch input mode"))
+                                        tooltip=_("Switch input mode"))
         self.__prop_dict[u"InputMode"] = input_mode_prop
 
         props = ibus.PropList()
         props.append(ibus.Property(key=u"InputMode.Hiragana",
                                    type=ibus.PROP_TYPE_RADIO,
-                                   label=_(u"Hiragana")))
+                                   label=_("Hiragana")))
         props.append(ibus.Property(key=u"InputMode.Katakana",
                                    type=ibus.PROP_TYPE_RADIO,
-                                   label=_(u"Katakana")))
+                                   label=_("Katakana")))
         props.append(ibus.Property(key=u"InputMode.HalfWidthKatakana",
                                    type=ibus.PROP_TYPE_RADIO,
-                                   label=_(u"Half width katakana")))
+                                   label=_("Half width katakana")))
         props.append(ibus.Property(key=u"InputMode.Latin",
                                    type=ibus.PROP_TYPE_RADIO,
-                                   label=_(u"Latin")))
+                                   label=_("Latin")))
         props.append(ibus.Property(key=u"InputMode.WideLatin",
                                    type=ibus.PROP_TYPE_RADIO,
-                                   label=_(u"Wide Latin")))
+                                   label=_("Wide Latin")))
 
         props[self.__input_mode].set_state(ibus.PROP_STATE_CHECKED)
 
@@ -170,19 +170,19 @@ class Engine(ibus.EngineBase):
         typing_mode_prop = ibus.Property(key=u"TypingMode",
                                          type=ibus.PROP_TYPE_MENU,
                                          label=u"R",
-                                         tooltip=_(u"Switch typing mode"))
+                                         tooltip=_("Switch typing mode"))
         self.__prop_dict[u"TypingMode"] = typing_mode_prop
 
         props = ibus.PropList()
         props.append(ibus.Property(key=u"TypingMode.Romaji",
                                    type=ibus.PROP_TYPE_RADIO,
-                                   label=_(u"Romaji")))
+                                   label=_("Romaji")))
         props.append(ibus.Property(key=u"TypingMode.Kana",
                                    type=ibus.PROP_TYPE_RADIO,
-                                   label=_(u"Kana")))
+                                   label=_("Kana")))
         props.append(ibus.Property(key=u"TypingMode.ThumbShift",
                                    type=ibus.PROP_TYPE_RADIO,
-                                   label=_(u"Thumb shift")))
+                                   label=_("Thumb shift")))
         props[Engine.__typing_mode].set_state(ibus.PROP_STATE_CHECKED)
 
         for prop in props:
@@ -192,7 +192,7 @@ class Engine(ibus.EngineBase):
         anthy_props.append(typing_mode_prop)
 
         anthy_props.append(ibus.Property(key=u"setup",
-                                         tooltip=_(u"Configure Anthy")))
+                                         tooltip=_("Configure Anthy")))
 
         return anthy_props
 
